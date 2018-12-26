900c08b6841246c4a9b7e729713d5e43daf177f9
keng000 <4s.flashback@gmail.com>
2018-12-26 22:54:54 +0900

    add type hinting and test.

--- app/controllers/s3_downloader.py
+++ app/controllers/s3_downloader.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import Union
 
 import boto3
+from botocore.client import BaseClient
 
 logger = getLogger(__name__)
 
@@ -22,7 +23,21 @@ def get_keys():
     return AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 
-def download(src_path_in_s3: Union[str, Path], dst_path_in_local: Union[str, Path], bucket_name: str):
+def create_session() -> BaseClient:
+    """
+    Create a session for aws s3.
+    Returns:
+        BaseClient: S3 Client
+    """
+    aws_access_key_id, aws_secret_access_key = get_keys()
+    client = boto3.client(
+        service_name='s3',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key)
+    return client
+
+
+def download(src_path_in_s3: Path, dst_path_in_local: Path, bucket_name: str = "keng000-mlops"):
     """
     download function from aws s3.
     Args:
@@ -30,21 +45,17 @@ def download(src_path_in_s3: Union[str, Path], dst_path_in_local: Union[str, Pat
         dst_path_in_local: Union[str, Path]: a path points a local path that the file will be saved.
         bucket_name: str: s3 bucket name.
     """
-    aws_access_key_id, aws_secret_access_key = get_keys()
-
-    client = boto3.client(
-        service_name='s3',
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key)
+    client = create_session()
 
     # TODO: add error handling(file not found in s3, client initializing error)
     logger.info(f"Start downloading: {dst_path_in_local}")
-    client.download_file(bucket_name, src_path_in_s3, dst_path_in_local)
+    dst_path_in_local.parent.mkdir(parents=True, exist_ok=True)
+    client.download_file(bucket_name, str(src_path_in_s3), str(dst_path_in_local))
     logger.info(f"File downloaded: {dst_path_in_local}")
 
 
 @contextmanager
-def download_as_temporary(src_path_in_s3: Union[str, Path], bucket_name: str) -> Path:
+def download_as_temporary(src_path_in_s3: Path, bucket_name: str = "keng000-mlops") -> Path:
     """
     This context manager downloads a file from s3 as temporary.
 
@@ -53,13 +64,18 @@ def download_as_temporary(src_path_in_s3: Union[str, Path], bucket_name: str) ->
     Returns:
         Path: temporary file path.
     """
-    tmp_file_path = tempfile.NamedTemporaryFile().name
+    tmp_file = tempfile.NamedTemporaryFile()
+    tmp_file_path = Path(tmp_file.name)
 
     download(src_path_in_s3, tmp_file_path, bucket_name)
 
     try:
-        yield Path(tmp_file_path)
+        yield tmp_file_path
 
     finally:
         logger.info(f"the temporary file removed: {tmp_file_path}")
-        tmp_file_path.close()
+        tmp_file.close()
+
+
+if __name__ == '__main__':
+    create_session()
